2cabee284aa62ca00e2586b49322724bd9f46e5a
ashkanMahdizadeh <ashkan.mahdizadeh.83@gmail.com>
2022-07-02 12:31:32 +0430

    edited owl slider

--- js/app.js
+++ js/app.js
@@ -87,6 +87,7 @@ $(document).ready(function(){
         loop:true,
         margin:10,
         responsiveClass:true,
+        rtl:true,
         responsive:{
             0:{
                 items:1,
