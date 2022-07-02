fb00f3e875c21433f3ac61e942a080e0a23f4666
ashkanMahdizadeh <ashkan.mahdizadeh.83@gmail.com>
2022-07-02 12:25:16 +0430

    add owl slider

--- js/app.js
+++ js/app.js
@@ -79,4 +79,28 @@ $(document).ready(function(){
         fullImage.attr('src', e.target.src)
         fullImage.attr('alt', e.target.alt)
     })
+
+
+
+    // owl slider
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:3,
+                nav:false
+            },
+            1000:{
+                items:5,
+                nav:true,
+                loop:false
+            }
+        }
+    })
 });
